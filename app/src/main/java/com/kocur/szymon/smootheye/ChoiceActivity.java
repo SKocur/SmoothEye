7dc266b5f10e05bb70534a16fd1cac6bc1f4d6fa
Szymon Kocur <skocur10@gmail.com>
2017-06-01 18:55:41 +0200

    Upload

---
+++ app/src/main/java/com/kocur/szymon/smootheye/ChoiceActivity.java
@@ -0,0 +1,151 @@
+package com.kocur.szymon.smootheye;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+public class ChoiceActivity extends AppCompatActivity {
+
+    private TextToSpeech tts;
+
+    Handler handler;
+    Button buttonChoice;
+
+    private volatile boolean isBackgroundThreadRunning;
+
+    HashMap<String, Integer> availableOptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+
+        buttonChoice = (Button) findViewById(R.id.buttonChoice);
+
+        availableOptions = new HashMap<String, Integer>();
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.US);
+                    tts.setSpeechRate(0.7f);
+                    speak("Please, choose your destination");
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "This Language is not supported");
+                    }
+                } else {
+                    Log.e("TTS", "Initilization Failed!");
+                }
+            }
+        });
+
+        Intent intent = getIntent();
+        final String intentData = intent.getStringExtra("data");
+        final String data[] = intentData.split("[-]");
+
+        handler = new Handler();
+
+        SpeakContent speakContent = new SpeakContent();
+        isBackgroundThreadRunning = true;
+        speakContent.start();
+        /*for(int i = 0; i < data.length; i++){
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            }, 500*i);
+        }*/
+        /*handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < data.length; i++){
+                    final int index = i;
+                    //For indexes like 2,4,6...
+                    //From SEMENU-1-Something-2-Something...
+                    if(i != 0 && i%2 == 0) {
+                        speak(data[i]);
+                        enableOptionOnButton(data[i]);
+                        Log.e(">>>>>>>>>>>>>>>>>>", data[i]);
+                        try {
+                            Thread.sleep(data[i].length() * 450);
+                        } catch (InterruptedException e) {
+
+                        }
+                    }
+                }
+                handler.postDelayed(this, 500);
+            }
+        }, 3000); */
+    }
+
+    private class SpeakContent extends Thread {
+        Intent intent = getIntent();
+        final String intentData = intent.getStringExtra("data");
+        final String data[] = intentData.split("[-]");
+        @Override
+        public void run(){
+            while(isBackgroundThreadRunning) {
+                for(int i = 1; i < data.length; i++){
+                    if(!isBackgroundThreadRunning)
+                        break;
+
+                    if(i%2 == 0) {
+                        availableOptions.put(data[i], Integer.parseInt(data[i-1]));
+                        speak(data[i]);
+                        enableOptionOnButton(data[i]);
+                        Log.e(">>>>>>>>>>>>>>>>>>", data[i-1]);
+                        try {
+                            Thread.sleep(data[i].length() * 450);
+                        } catch (InterruptedException e) {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void enableOptionOnButton(final String option){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                buttonChoice.setText(option);
+                buttonChoice.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        isBackgroundThreadRunning = false;
+                        tts.shutdown();
+                        handler.removeCallbacksAndMessages(null);
+                        Intent intent = new Intent();
+                        intent.putExtra("data", availableOptions.get(option).toString());
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                });
+            }
+        });
+    }
+
+    public void speak(String text){
+        if(!tts.isSpeaking()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+            else
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+}
