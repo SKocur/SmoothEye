16fb8331c7995e931040b60f28e6dfc4634ff150
Szymon Kocur <skocur10@gmail.com>
2018-03-02 23:04:56 +0100

    White screen onBackPressed bug fix

--- app/src/main/java/com/kocur/szymon/smootheye/SwipeCore.java
+++ app/src/main/java/com/kocur/szymon/smootheye/SwipeCore.java
@@ -4,7 +4,6 @@ import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
